6a9520c7399f5fd58861b4408e7e3a793486956e
Pedro Dias <pedrodiasfonseca@gmail.com>
2014-09-22 23:55:11 -0300

    Configuração inicial do projeto

---
+++ app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name projetobrasil.ufc
+ * @description
+ * # projetobrasil.ufc
+ *
+ * Main module of the application.
+ */
+angular
+  .module('projetobrasil.ufc', [
+    'ngResource',
+    'ngSanitize'
+  ]);
