4956d0bc04f49122bf255de04354932f8b833390
Augusto Farnese <augustofarnese@gmail.com>
2014-09-29 20:41:22 -0300

    Terminando a barra. Ainda faltam detalhes esteticos

--- app/modules/interface/interface.ctrl.js
+++ app/modules/interface/interface.ctrl.js
@@ -28,17 +28,17 @@ angular.module('projetobrasil.ufc.interface.controllers', [
 		x2 = '40';
 		y2 = '34';
 
-		x3a = 45.4*($scope.maxGolpesPorRound - $scope.p1.golpesSofridos + 1);
+		x3a = 40*($scope.maxGolpesPorRound - $scope.p1.golpesSofridos + 1);
 		y3a = -0.075*x3a+37.78;
 		// y3 = '0';
 
-		x4a = x3a - 50;
+		x4a = x3a - 30;
 		y4a = '50';
 
-		x4b = 45.4*($scope.maxGolpesPorRound - $scope.p2.golpesSofridos + 1);
+		x4b = 40*($scope.maxGolpesPorRound - $scope.p2.golpesSofridos + 1);
 		y4b = '50';
 
-		x3b = x4b - 50;
+		x3b = x4b - 30;
 		y3b = -0.075*x3b+37.78;
 		// y3 = '0';
 
@@ -50,21 +50,21 @@ angular.module('projetobrasil.ufc.interface.controllers', [
 
     $scope.$watch('p1.golpesSofridos', function(){
     	console.log('Mudou!');
-			x3a = 45.4*($scope.maxGolpesPorRound - $scope.p1.golpesSofridos + 1);
+			x3a = 40*($scope.maxGolpesPorRound - $scope.p1.golpesSofridos + 1);
 			y3a = -0.075*x3a+37.78;
 			// y3 = '0';
 
-			x4a = x3a - 50;
+			x4a = x3a - 30;
 			y4a = '50';
     	$scope.coordenadasA = x1 + ',' + y1 + ' ' + x2 + ',' + y2 + ' ' + x3a + ',' + y3a + ' ' + x4a + ',' + y4a ;
     })
 
     $scope.$watch('p2.golpesSofridos', function(){
     	console.log('Mudou!');
-			x4b = 45.4*($scope.maxGolpesPorRound - $scope.p2.golpesSofridos + 1);
+			x4b = 40*($scope.maxGolpesPorRound - $scope.p2.golpesSofridos + 1);
 			y4b = '50';
 
-			x3b = x4b - 50;
+			x3b = x4b - 30;
 			y3b = -0.075*x3b+37.78;
 
 			if($scope.p2.golpesSofridos == $scope.maxGolpesPorRound){
