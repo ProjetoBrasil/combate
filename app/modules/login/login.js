08b147000266f39b8f337263117c900d58c97eb7
Augusto Farnese <augustofarnese@gmail.com>
2014-10-01 19:19:23 -0300

    Login funciona, enviando cookies

--- app/modules/login/login.js
+++ app/modules/login/login.js
@@ -3,16 +3,13 @@
 angular
 	.module('projetobrasil.ufc.login', [
 		'projetobrasil.ufc.login.controllers',
-		'projetobrasil.ufc.login.services',
-		'facebook'
-	]).config(function(FacebookProvider) {
-     // Set your appId through the setAppId method or
-     // use the shortcut in the initialize method directly.
-     FacebookProvider.init('1520150961538128');
-  }).config(function($stateProvider){
+		'projetobrasil.ufc.login.services'
+	]).config(function($stateProvider){
 		$stateProvider
 				.state('login', {
 					url: '/login',
 					templateUrl: 'modules/login/login.html'
 				});
-	});
+	}).config(['$httpProvider', function ($httpProvider) {
+	  $httpProvider.defaults.withCredentials = true;
+	}]);;
