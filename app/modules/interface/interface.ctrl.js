8d9a5f647b21b8c7af0f1b04275270c1f7e905d4
Pedro Dias <pedrodiasfonseca@gmail.com>
2014-10-01 17:24:27 -0300

    Muda o posicionamento da imagem de fundo e redimensiona o canvas de acordo com a tela

--- app/modules/interface/interface.ctrl.js
+++ app/modules/interface/interface.ctrl.js
@@ -3,7 +3,8 @@
 angular.module('projetobrasil.ufc.interface.controllers', [
 		'projetobrasil.ufc.jogo.services'
 	])
-	.controller('InterfaceCtrl', [ function (){
+	.controller('InterfaceCtrl', ['$scope', '$window', function ($scope, $window){
+		$scope.canvasWidth = $window.innerWidth > 1280 ? 1280 : $window.innerWidth;
 	}])
 	.controller('CandidatosCtrl', [ function (){
 	}])
