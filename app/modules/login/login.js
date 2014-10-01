e2758574866ccda4549077f58c4f0ef8c17df902
Augusto Farnese <augustofarnese@gmail.com>
2014-10-01 19:25:22 -0300

    Deixa só o serviço do login

--- app/modules/login/login.js
+++ app/modules/login/login.js
@@ -2,14 +2,7 @@
 
 angular
 	.module('projetobrasil.ufc.login', [
-		'projetobrasil.ufc.login.controllers',
 		'projetobrasil.ufc.login.services'
-	]).config(function($stateProvider){
-		$stateProvider
-				.state('login', {
-					url: '/login',
-					templateUrl: 'modules/login/login.html'
-				});
-	}).config(['$httpProvider', function ($httpProvider) {
+	]).config(['$httpProvider', function ($httpProvider) {
 	  $httpProvider.defaults.withCredentials = true;
 	}]);;
