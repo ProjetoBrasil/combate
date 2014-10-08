1fc03f6403146f28cb31f2b2a9799e74dfb81a5a
Augusto Farnese <augustofarnese@gmail.com>
2014-10-08 17:35:02 -0300

    Removido controlador não utilizado

--- app/modules/interface/interface.ctrl.js
+++ app/modules/interface/interface.ctrl.js
@@ -4,13 +4,6 @@ angular.module('projetobrasil.ufc.interface.controllers', [])
 	.controller('InterfaceCtrl', [function (){
 
 	}])
-	.controller('BarrasCtrl', ['$scope', '$rootScope', 'GerenciadorJogo', function ($scope, $rootScope, Jogo){
-		var cand = $rootScope.idsCandidatos;
-		$scope.p1 = Jogo.candidatos[cand[0]];
-		$scope.p2 = Jogo.candidatos[cand[1]];
-		$scope.maxGolpesPorRound = Jogo.maxGolpesRound;
-
-	}])
 	.controller('barraDeVidaCtrl', ['$scope', '$rootScope', 'GerenciadorJogo', function($scope, $rootScope, Jogo) {
 		var cand = $rootScope.idsCandidatos;
 		$scope.p1 = Jogo.candidatos[cand[0]];
